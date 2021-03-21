408599ce0440d83232bd60a11db11380a76e0022
Kaeldehta <floxx33@gmail.com>
2021-03-21 18:29:05 +0100

    Added gamesettings script to host gamesettings like grav constant

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+
+    private static GameSettings _settingsSingleton;
+
+    public static GameSettings Instance => _settingsSingleton;
+
+    [SerializeField] private float gravitationalConstant;
+
+    public float GravitationalConstant => gravitationalConstant;
+
+    void Start()
+    {
+        if (_settingsSingleton == null)
+        {
+            _settingsSingleton = this;
+        }
+    }
+
+}
