10e6eb85bf9c617c4019a0309de8afc2d70dea5e
Kaeldehta <floxx33@gmail.com>
2021-03-21 18:29:54 +0100

    Added golfable script for golf ball like objects

---
+++ Assets/Scripts/Golfable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golfable : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        Stop();
+    }
+
+
+    public void Stop()
+    {
+        _rigidbody.isKinematic = true;
+    }
+
+    public void Hit(Vector3 initialVelocity)
+    {
+        _rigidbody.velocity = initialVelocity;
+        _rigidbody.isKinematic = false;
+    }
+}
