0ff19796f634b24525c79a296f0aab8f6b07bb19
Kaeldehta <floxx33@gmail.com>
2021-04-09 07:48:06 +0200

    Update Golfable.cs

--- Assets/Scripts/Golfable.cs
+++ Assets/Scripts/Golfable.cs
@@ -1,27 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Golfable : MonoBehaviour
 {
 
     private Rigidbody _rigidbody;
     
-    private void Start()
+    private float _timer;
+    private bool _updateTimer;
+
+    private Vector3 _originPos;
+
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        Stop();
+
+        GameStateManager.Instance.ONBallMovementEnter += initVelocity =>
+        {
+            _originPos = transform.position;
+            _rigidbody.velocity = initVelocity;
+            _timer = 0;
+        };
+        
+        GameStateManager.Instance.ONInputEnter += () => _rigidbody.velocity = Vector3.zero;
     }
 
-
-    public void Stop()
+    private void OnCollisionEnter(Collision other)
     {
-        _rigidbody.isKinematic = true;
+        _updateTimer = false;
+        _timer = 0;
     }
 
-    public void Hit(Vector3 initialVelocity)
+    private void OnCollisionExit(Collision other)
     {
-        _rigidbody.isKinematic = false;
-        _rigidbody.AddForce(initialVelocity, ForceMode.Impulse);
+        _updateTimer = true;
+    }
+
+    private void LateUpdate()
+    {
+        if (GameStateManager.Instance.CurrentState != GameStateManager.GameState.BallMovement) return;
+        
+        if(_updateTimer) _timer += Time.deltaTime;
+        
+        if (_rigidbody.velocity.sqrMagnitude < 0.07f) GameStateManager.Instance.StartInput();
+        else if (_timer >= 5f)
+        {
+            transform.position = _originPos;
+            
+            GameStateManager.Instance.StartInput();
+        }
+
+        
     }
 }
