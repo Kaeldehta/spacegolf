38ebff5cef291af5fc12883e37f774c3563ca898
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:04:43 +0200

    toggle how to ui

---
+++ Assets/Scripts/ToggleHowToUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleHowToUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameManager.Instance.ONMainMenuEnter += () => gameObject.SetActive(false);
+        GameManager.Instance.ONHowToEnter += () => gameObject.SetActive(true);
+    }
+}
