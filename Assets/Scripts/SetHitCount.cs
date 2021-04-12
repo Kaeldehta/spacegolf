d69dbee10f26c86346e8d15564f894e8befd269b
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:04:11 +0200

    sethitcount script

---
+++ Assets/Scripts/SetHitCount.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SetHitCount : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        var text = GetComponent<TextMeshProUGUI>();
+        GameManager.Instance.ONLevelStartExit += () => text.text = GameManager.Instance.NumberOfHits.ToString();
+        GameManager.Instance.ONBallMovementEnter += velo => text.text = GameManager.Instance.NumberOfHits.ToString();
+    }
+}
