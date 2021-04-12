7373ea2766df73e9149e0117eb1227616f805b2c
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:04:55 +0200

    toggle level done ui

---
+++ Assets/Scripts/ToggleLevelDoneUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLevelDoneUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameManager.Instance.ONLevelCompleted += () => gameObject.SetActive(true);
+        GameManager.Instance.ONMainMenuEnter += () => gameObject.SetActive(false);
+    }
+}
