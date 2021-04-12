05f7eeb663f49f6c6b064f6eec9a777e4eee4c39
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:05:41 +0200

    toggle planet camera ui

---
+++ Assets/Scripts/TogglePlanetCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TogglePlanetCamera : MonoBehaviour
+{
+    private Action handler1;
+    private Action handler2;
+
+
+    private void Awake()
+    {
+        handler1 = () => gameObject.SetActive(false);
+        GameManager.Instance.ONMainMenuEnter += handler1;
+        handler2 = () => gameObject.SetActive(true);
+        GameManager.Instance.ONLevelStartExit += handler2;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.ONMainMenuEnter -= handler1;
+        GameManager.Instance.ONLevelStartExit -= handler2;
+    }
+}
