d0e3db3084de8d274d068bcf8c9a4997babecaa2
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:05:19 +0200

    toggle level ui

---
+++ Assets/Scripts/ToggleLevelUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLevelUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameManager.Instance.ONLevelStartExit += () => gameObject.SetActive(true);
+        GameManager.Instance.ONMainMenuEnter += () => gameObject.SetActive(false);
+    }
+}
