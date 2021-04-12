e85df959c16b28c79d26b7c0f5bdd3c001d855b0
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:05:29 +0200

    toggle main menu ui

---
+++ Assets/Scripts/ToggleMainMenuUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleMainMenuUI : MonoBehaviour
+{
+    void Awake()
+    {
+        GameManager.Instance.ONMainMenuEnter += () => gameObject.SetActive(true);
+        GameManager.Instance.ONLevelSelectEnter += () => gameObject.SetActive(false);
+        GameManager.Instance.ONHowToEnter += () => gameObject.SetActive(false);
+    }
+}
