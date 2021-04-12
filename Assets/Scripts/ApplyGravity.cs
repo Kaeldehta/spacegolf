04987dbda119a3cd47e07d9453b03f6a1ad39fe9
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:00:02 +0200

    Update ApplyGravity.cs

--- Assets/Scripts/ApplyGravity.cs
+++ Assets/Scripts/ApplyGravity.cs
@@ -11,25 +11,33 @@ public class ApplyGravity : MonoBehaviour
     private Rigidbody _rigidbody;
     private Rigidbody _planetRb;
 
+    private Action handler1;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
 
-        GameStateManager.Instance.ONLevelStartExit += () => {
-            Debug.Log("ApplyGrav");
-            _planetRb = GameStateManager.Instance.Planet.GetComponent<Rigidbody>();
-         };
+        handler1 = () => {
+            _planetRb = GameManager.Instance.Planet.GetComponent<Rigidbody>();
+        };
+
+        GameManager.Instance.ONLevelStartExit += handler1;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.ONLevelStartExit -= handler1;
     }
 
     private void FixedUpdate()
     {
-        if (GameStateManager.Instance.CurrentState != GameStateManager.GameState.BallMovement) return;
+        if (GameManager.Instance.CurrentState != GameManager.GameState.BallMovement) return;
 
         // Calculate vector between this object and target
         Vector3 between = _planetRb.transform.position - transform.position;
 
         // Calculate the acceleration that needs to be applied to this GameObject
-        Vector3 acceleration = GameSettings.Instance.GravitationalConstant * _planetRb.mass /
+        Vector3 acceleration = GameManager.Instance.GravitationalConstant * _planetRb.mass /
             between.sqrMagnitude * between.normalized;
 
         // Apply acceleration to rigidbody
