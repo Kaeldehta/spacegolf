e54989185ab108320f6051fcf733f111d5ea0c57
Kaeldehta <floxx33@gmail.com>
2021-04-12 20:04:25 +0200

    toggle camera script

---
+++ Assets/Scripts/ToggleCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCamera : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameManager.Instance.ONMainMenuEnter += () => gameObject.SetActive(true);
+        GameManager.Instance.ONLevelStartExit += () => gameObject.SetActive(false);
+    }
+}
